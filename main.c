3e634953bfc34685e3e685392f3c258da648215a
Sholde <exshode@gmail.com>
2021-04-07 12:32:26 +0200

    change double to float and extand to float8

--- main.c
+++ main.c
@@ -2,16 +2,17 @@
 
 #include "rdtsc.h"
 
-#define N_MAX 1000
+#define N_MAX 10000
 
-typedef double double2 __attribute__((ext_vector_type(2)));
-typedef double double4 __attribute__((ext_vector_type(4)));
+typedef float float2 __attribute__((ext_vector_type(2)));
+typedef float float4 __attribute__((ext_vector_type(4)));
+typedef float float8 __attribute__((ext_vector_type(8)));
 
 void run_addition(void)
 {
-  double a = 1.0;
-  double b = 1.0;
-  double c = 0.0;
+  float a = 1.0;
+  float b = 1.0;
+  float c = 0.0;
 
   double start, end, final = 0;
 
@@ -25,16 +26,18 @@ void run_addition(void)
       
       if (start < end)
         final += end - start;
+      else
+        i--;
     }
 
-  fprintf(stderr, "addition;  double; %f\n", final);
+  fprintf(stderr, "addition;  float; %f\n", final);
 }
 
 void run_addition_2x(void)
 {
-  double2 a = 1.0;
-  double2 b = 1.0;
-  double2 c = 0.0;
+  float2 a = 1.0;
+  float2 b = 1.0;
+  float2 c = 0.0;
 
   double start, end, final = 0;
 
@@ -48,16 +51,18 @@ void run_addition_2x(void)
       
       if (start < end)
         final += end - start;
+      else
+        i--;
     }
 
-  fprintf(stderr, "addition; double2; %f\n", final);
+  fprintf(stderr, "addition; float2; %f\n", final);
 }
 
 void run_addition_4x(void)
 {
-  double2 a = 1.0;
-  double2 b = 1.0;
-  double2 c = 0.0;
+  float4 a = 1.0;
+  float4 b = 1.0;
+  float4 c = 0.0;
 
   double start, end, final = 0;
 
@@ -71,16 +76,43 @@ void run_addition_4x(void)
       
       if (start < end)
         final += end - start;
+      else
+        i--;
     }
 
-  fprintf(stderr, "addition; double4; %f\n", final);
+  fprintf(stderr, "addition; float4; %f\n", final);
+}
+
+void run_addition_8x(void)
+{
+  float8 a = 1.0;
+  float8 b = 1.0;
+  float8 c = 0.0;
+
+  double start, end, final = 0;
+
+  for (int i = 0; i < N_MAX / 8; i++)
+    {
+      start = rdtsc();
+      {
+        c = a + b;
+      }
+      end = rdtsc();
+      
+      if (start < end)
+        final += end - start;
+      else
+        i--;
+    }
+
+  fprintf(stderr, "addition; float4; %f\n", final);
 }
 
 int main(int argc, char **argv)
 {
-  run_addition_4x();
-  run_addition_2x();
   run_addition();
-  
+  run_addition_2x();
+  run_addition_4x();
+  run_addition_8x();  
   return 0;
 }
