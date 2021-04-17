1cf4168bc6bb566f6d2b7a9d3e7867faf42d046c
Sholde <exshode@gmail.com>
2021-04-17 23:00:42 +0200

    adding core taskset in argument

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Recup the core
+core=$1
+
 # clean
 make clean
 
@@ -11,7 +14,7 @@ export VFC_BACKENDS_SILENT_LOAD="True"
 export VFC_BACKENDS_LOGGER="False"
 
 # ieee
-VFC_BACKENDS="libinterflop_ieee.so" taskset -c 1 ./main 1.0 1.0 2> ieee_tmp.txt
+VFC_BACKENDS="libinterflop_ieee.so" taskset -c $core ./main 1.0 1.0 2> ieee_tmp.txt
 cat ieee_tmp.txt | sed -n "1,4p" > ieee_perf.txt
 cat ieee_tmp.txt | sed -n "5,7p" > ieee_speedup.txt
 cat ieee_tmp.txt | sed -n "8,11p" >> ieee_perf.txt
@@ -22,7 +25,7 @@ cat ieee_tmp.txt | sed -n "22,25p" >> ieee_perf.txt
 cat ieee_tmp.txt | sed -n "26,28p" >> ieee_speedup.txt
 
 # vprec normal
-VFC_BACKENDS="libinterflop_vprec.so" taskset -c 1 ./main 1.0 1.0 2> vprec_normal_tmp.txt
+VFC_BACKENDS="libinterflop_vprec.so" taskset -c $core ./main 1.0 1.0 2> vprec_normal_tmp.txt
 cat vprec_normal_tmp.txt | sed -n "1,4p" > vprec_normal_perf.txt
 cat vprec_normal_tmp.txt | sed -n "5,7p" > vprec_normal_speedup.txt
 cat vprec_normal_tmp.txt | sed -n "8,11p" >> vprec_normal_perf.txt
@@ -33,7 +36,7 @@ cat vprec_normal_tmp.txt | sed -n "22,25p" >> vprec_normal_perf.txt
 cat vprec_normal_tmp.txt | sed -n "26,28p" >> vprec_normal_speedup.txt
 
 # vprec denormal
-VFC_BACKENDS="libinterflop_vprec.so" taskset -c 1 ./main 0x1.0p-130 0x1.0p-130 2> vprec_denormal_tmp.txt
+VFC_BACKENDS="libinterflop_vprec.so" taskset -c $core ./main 0x1.0p-130 0x1.0p-130 2> vprec_denormal_tmp.txt
 cat vprec_denormal_tmp.txt | sed -n "1,4p" > vprec_denormal_perf.txt
 cat vprec_denormal_tmp.txt | sed -n "5,7p" > vprec_denormal_speedup.txt
 cat vprec_denormal_tmp.txt | sed -n "8,11p" >> vprec_denormal_perf.txt
@@ -44,7 +47,7 @@ cat vprec_denormal_tmp.txt | sed -n "22,25p" >> vprec_denormal_perf.txt
 cat vprec_denormal_tmp.txt | sed -n "26,28p" >> vprec_denormal_speedup.txt
 
 # vprec full normal
-VFC_BACKENDS="libinterflop_vprec.so --mode=full" taskset -c 1 ./main 1.0 1.0 2> vprec_full_normal_tmp.txt
+VFC_BACKENDS="libinterflop_vprec.so --mode=full" taskset -c $core ./main 1.0 1.0 2> vprec_full_normal_tmp.txt
 cat vprec_full_normal_tmp.txt | sed -n "1,4p" > vprec_full_normal_perf.txt
 cat vprec_full_normal_tmp.txt | sed -n "5,7p" > vprec_full_normal_speedup.txt
 cat vprec_full_normal_tmp.txt | sed -n "8,11p" >> vprec_full_normal_perf.txt
@@ -55,7 +58,7 @@ cat vprec_full_normal_tmp.txt | sed -n "22,25p" >> vprec_full_normal_perf.txt
 cat vprec_full_normal_tmp.txt | sed -n "26,28p" >> vprec_full_normal_speedup.txt
 
 # vprec full denormal
-VFC_BACKENDS="libinterflop_vprec.so --mode=full" taskset -c 1 ./main 0x1.0p-130 0x1.0p-130 2> vprec_full_denormal_tmp.txt
+VFC_BACKENDS="libinterflop_vprec.so --mode=full" taskset -c $core ./main 0x1.0p-130 0x1.0p-130 2> vprec_full_denormal_tmp.txt
 cat vprec_full_denormal_tmp.txt | sed -n "1,4p" > vprec_full_denormal_perf.txt
 cat vprec_full_denormal_tmp.txt | sed -n "5,7p" > vprec_full_denormal_speedup.txt
 cat vprec_full_denormal_tmp.txt | sed -n "8,11p" >> vprec_full_denormal_perf.txt
